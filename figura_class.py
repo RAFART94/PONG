61f69a212ea3dee565a20202ae090c1be67ddc2b
RAFART94 <danielgrs94@gmail.com>
2023-12-02 21:37:44 +0000

    Agregar la función mover a la clase Pelota, agregado contador de marcador y uso de Font

--- figura_class.py
+++ figura_class.py
@@ -1,14 +1,13 @@
 import pygame as pg
+import random as ra
 
 class Raqueta():
-    def __init__(self, pos_x, pos_y, color=(255,255,255),w=20, h=120,vx=0, vy=0):
+    def __init__(self, pos_x, pos_y, color=(255,255,255),w=20, h=120,):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.color = color
         self.w = w
         self.h = h
-        self.vx = vx
-        self.vy = vy
 
     def dibujar(self, surface):
         pg.draw.rect(surface, self.color, (self.pos_x-(self.w//2), self.pos_y-(self.h//2), self.w, self.h))
@@ -24,11 +23,35 @@ class Raqueta():
     
 
 class Pelota():
-    def __init__(self, pos_x, pos_y, color=(255,255,255), radio=5):
+    def __init__(self, pos_x, pos_y, color=(255,255,255), radio=5, vx=1, vy=1):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.color = color
         self.radio = radio
+        self.vx = vx
+        self.vy = vy
+        self.contadorDerecho = 0
+        self.contadorIzquierdo = 0
 
     def dibujar(self, surface):
         pg.draw.circle(surface, self.color, (self.pos_x, self.pos_y), self.radio)
+
+    def mover(self, x_max=800, y_max=600):
+        self.pos_x += self.vx
+        self.pos_y += self.vy
+        #limite derecho
+        if self.pos_x >= x_max + (5*self.radio):
+            self.pos_x = 400
+            self.pos_y = 300
+            self.vx *= -1
+            self.contadorDerecho += 1
+
+        #limite izquierdo
+        if self.pos_x <= 0 - (5*self.radio):
+            self.pos_x = 400
+            self.pos_y = 300
+            self.vx *= -1
+            self.contadorIzquierdo += 1
+
+        if self.pos_y >= y_max or self.pos_y <= 0:
+            self.vy *= -1
--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import pygame as pg
 from figura_class import Pelota, Raqueta
 
 pg.init()
-X_MAX = 800
-Y_MAX = 600
+
 pantalla_principal = pg.display.set_mode((800,600))
 pg.display.set_caption('Pong')
 
 #Definir tasa de refresco en nuestro bucle de fotogramas, fps= fotograma por segundo
 tasa_refresco = pg.time.Clock()
+
+#AGREGAR MARCADORES
+#Asignación de fuente y letra
+marcador1_font = pg.font.SysFont("Verdana", 30)
+marcador2_font = pg.font.SysFont('Verdana', 30)
+player1_font = pg.font.SysFont('Verdana', 30)
+player2_font = pg.font.SysFont('Verdana', 30)
 
 #Creamos un objeto de la clase Pelota o instanciamos la clase Pelota
 pelota = Pelota(400, 300, (228,231,19), 15)
@@ -31,12 +37,26 @@ while game_over:
 
     pg.draw.line(pantalla_principal, (255,255,255), (400,0), (400,600), width=10)
 
-    raqueta1.mover(pg.K_w, pg.K_s)
-    raqueta2.mover(pg.K_UP, pg.K_DOWN)
-
-    pelota.dibujar(pantalla_principal)
     raqueta1.dibujar(pantalla_principal)
     raqueta2.dibujar(pantalla_principal)
+    pelota.dibujar(pantalla_principal)
+
+    raqueta1.mover(pg.K_w, pg.K_s)
+    raqueta2.mover(pg.K_UP, pg.K_DOWN)
+    pelota.mover()
+    print('Jugador Izquierdo: ',pelota.contadorIzquierdo)
+    print('Jugador Derecho: ',pelota.contadorDerecho)
+
+    #Asignación de color y texto
+    marcador1 = marcador1_font.render(str(pelota.contadorIzquierdo), True, (255,255,255))
+    marcador2 = marcador2_font.render(str(pelota.contadorDerecho), True, (255,255,255))
+    player1 = player1_font.render('Player 1', True, (255,255,255))
+    player2 = player2_font.render('Player 2', True, (255,255,255))
+
+    pantalla_principal.blit(marcador1, (325,50))
+    pantalla_principal.blit(marcador2, (450,50))
+    pantalla_principal.blit(player1, (225,20))
+    pantalla_principal.blit(player2, (450,20))
 
     pg.display.flip()
 
