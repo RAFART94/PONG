f8067051330d126d8ae0fa8c682b81f96b2fc3d5
RAFART94 <danielgrs94@gmail.com>
2023-12-02 20:04:22 +0000

    Agregado la función mover en Raqueta

--- figura_class.py
+++ figura_class.py
@@ -1,16 +1,27 @@
 import pygame as pg
 
 class Raqueta():
-    def __init__(self, pos_x, pos_y, color=(255,255,255),w=20, h=120):
+    def __init__(self, pos_x, pos_y, color=(255,255,255),w=20, h=120,vx=0, vy=0):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.color = color
         self.w = w
         self.h = h
+        self.vx = vx
+        self.vy = vy
 
     def dibujar(self, surface):
         pg.draw.rect(surface, self.color, (self.pos_x-(self.w//2), self.pos_y-(self.h//2), self.w, self.h))
 
+    def mover(self, teclado_arriba, teclado_abajo):
+        estado_teclado = pg.key.get_pressed()
+
+        if estado_teclado[teclado_arriba] == True and self.pos_y >= 0 + (self.h//2):
+            self.pos_y -= 1
+
+        if estado_teclado[teclado_abajo] == True and  self.pos_y <= 600 - (self.h//2):
+            self.pos_y += 1
+    
 
 class Pelota():
     def __init__(self, pos_x, pos_y, color=(255,255,255), radio=5):
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ X_MAX = 800
 Y_MAX = 600
 pantalla_principal = pg.display.set_mode((800,600))
 pg.display.set_caption('Pong')
+
 #Definir tasa de refresco en nuestro bucle de fotogramas, fps= fotograma por segundo
 tasa_refresco = pg.time.Clock()
 
@@ -20,25 +21,18 @@ game_over = True
 
 while game_over:
     #Obtener la tasa de refresco en milisegundos
-    valor_tasa = tasa_refresco.tick(300)#Variable para controlar la velocidad entre fotogramas
+    valor_tasa = tasa_refresco.tick(350)#Variable para controlar la velocidad entre fotogramas
 
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
             game_over = False
 
-    estado_teclado = pg.key.get_pressed()
-   #print('Estado teclado: ',estado_teclado[pg.K_UP])
-
-    if estado_teclado[pg.K_UP] == True:
-        raqueta1.pos_y -= 1
-
-    if estado_teclado[pg.K_DOWN] == True:
-        raqueta1.pos_y += 1
-    
-
     pantalla_principal.fill((27,149,47))
 
     pg.draw.line(pantalla_principal, (255,255,255), (400,0), (400,600), width=10)
+
+    raqueta1.mover(pg.K_w, pg.K_s)
+    raqueta2.mover(pg.K_UP, pg.K_DOWN)
 
     pelota.dibujar(pantalla_principal)
     raqueta1.dibujar(pantalla_principal)
