32fc01d92a2f5ae92c5fac5bc81eda669ceb12ca
RAFART94 <danielgrs94@gmail.com>
2023-12-02 19:14:36 +0000

    Evento de teclado y uso de tasa de refresco

--- figura_class.py
+++ figura_class.py
@@ -1,7 +1,7 @@
 import pygame as pg
 
 class Raqueta():
-    def __init__(self, pos_x, pos_y, color=(255,255,255),w=5, h=50):
+    def __init__(self, pos_x, pos_y, color=(255,255,255),w=20, h=120):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.color = color
@@ -9,7 +9,7 @@ class Raqueta():
         self.h = h
 
     def dibujar(self, surface):
-        pg.draw.rect(surface, self.color, (self.pos_x, self.pos_y, self.w, self.y))
+        pg.draw.rect(surface, self.color, (self.pos_x-(self.w//2), self.pos_y-(self.h//2), self.w, self.h))
 
 
 class Pelota():
--- main.py
+++ main.py
@@ -1,29 +1,50 @@
 #Aquí se ejecuta todo
 import pygame as pg
-from figura_class import Pelota
+from figura_class import Pelota, Raqueta
 
 pg.init()
-
+X_MAX = 800
+Y_MAX = 600
 pantalla_principal = pg.display.set_mode((800,600))
 pg.display.set_caption('Pong')
+#Definir tasa de refresco en nuestro bucle de fotogramas, fps= fotograma por segundo
+tasa_refresco = pg.time.Clock()
 
 #Creamos un objeto de la clase Pelota o instanciamos la clase Pelota
 pelota = Pelota(400, 300, (228,231,19), 15)
 
+raqueta1 = Raqueta(10, 300)#Raqueta izquierda
+raqueta2 = Raqueta(790, 300)#Raqueta derecha
+
 game_over = True
 
 while game_over:
+    #Obtener la tasa de refresco en milisegundos
+    valor_tasa = tasa_refresco.tick(300)#Variable para controlar la velocidad entre fotogramas
 
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
             game_over = False
+
+    estado_teclado = pg.key.get_pressed()
+   #print('Estado teclado: ',estado_teclado[pg.K_UP])
+
+    if estado_teclado[pg.K_UP] == True:
+        raqueta1.pos_y -= 1
+
+    if estado_teclado[pg.K_DOWN] == True:
+        raqueta1.pos_y += 1
+    
 
     pantalla_principal.fill((27,149,47))
 
     pg.draw.line(pantalla_principal, (255,255,255), (400,0), (400,600), width=10)
 
     pelota.dibujar(pantalla_principal)
+    raqueta1.dibujar(pantalla_principal)
+    raqueta2.dibujar(pantalla_principal)
 
     pg.display.flip()
+
 
 pg.quit()
